6f82201322541c5c85b712131d441ad67e57afd3
MVadia <81384379+MVadia@users.noreply.github.com>
2022-03-21 18:11:23 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CMP1903M_Assessment_1_Base_Code.Input;
+using static CMP1903M_Assessment_1_Base_Code.Analyse;
+using static CMP1903M_Assessment_1_Base_Code.Report;
+
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+            //initialise variables
+            List<int> values = null; //list to hold values
+            string input=("nothing"); //input string 
+
+            //Create 'Input' and 'Analyse' object
+            Input p = new Input();
+            Analyse a = new Analyse();
+
+            //Get either manually entered text, or text from a file
+            Console.WriteLine("1: Enter '1' to enter text via keyboard.");
+            Console.WriteLine("2: Enter '2' to read text from file.");
+            
+            //loop until either 1 or 2 is entered    
+            bool inputValid = true;
+            do
+            {
+                try
+                {
+                    int inputChoice = Convert.ToInt32(Console.ReadLine());
+                    if (inputChoice==1)
+                    {
+                        inputValid=false;
+                        input = p.manualTextInput();//get manual input
+                        values =a.analyseText(input);//pass input to 'analyse' method, receive list of values back
+                        break;
+                
+                    }
+                    if (inputChoice==2)
+                    {
+                        inputValid = false;
+                        input = p.fileTextInput();//get file input
+                        values =a.analyseText(input);//pass file text to 'analyse' method, receive list of values back
+                        break;
+            
+                            
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter either '1' or '2'.");
+                    }
+                }
+                catch{
+                    Console.WriteLine("Enter either '1' or '2'");
+                }
+            }while(inputValid == true);
+
+                
+                
+                
+                
+                
+
+            
+
+            //create 'Report' object and receive individual frequencies
+            Report r = new Report();
+            int sentenceCount = r.sentenceCountMethod(values);
+            int vowelCount = r.vowelCountMethod(values);
+            int consonantCount = r.consonantCountMethod(values);
+            int upperCount = r.upperCountMethod(values);
+            int lowerCount = r.lowerCountMethod(values);
+
+            //print frequencies to user
+            Console.WriteLine("Number of sentences: "+sentenceCount);
+            Console.WriteLine("Number of vowels: "+vowelCount);
+            Console.WriteLine("Number of consonents: "+consonantCount);
+            Console.WriteLine("Number of upper case letters: "+upperCount);
+            Console.WriteLine("Number of lower case letters "+ lowerCount);
+
+
+
+
+
+
+
+            //TO ADD: Get the frequency of individual letters?
+
+           
+        }
+        
+        
+    
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CMP1903M_Assessment_1_Base_Code.Analyse;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Report
+    {
+        //Handles the reporting of the analysis, isolates specific values from 'Value' list
+
+
+        //handles sentence count
+        //Argument: List of values
+        //Return: Integer 
+        public int sentenceCountMethod(List<int>values)
+        {
+            
+            int sentenceCount = values[0];
+            return sentenceCount;
+            
+        }
+
+        //handles vowel count
+        //Argument: List of values
+        //Return: Integer 
+        public int vowelCountMethod(List<int>values)
+        {
+            int vowelCount = values[1];
+            return vowelCount;
+        }
+
+        //handles consonent count
+        //Argument: List of values
+        //Return: Integer 
+        public int consonantCountMethod(List<int>values)
+        {
+            int consonantCount = values[2];
+            return consonantCount;
+        }
+
+        //handles upper case letters count
+        //Argument: List of values
+        //Return: Integer 
+        public int upperCountMethod(List<int>values)
+        {
+            int upperCount = values[3];
+            return upperCount;
+        }
+
+        //handles lower case letter count
+        //Argument: List of values
+        //Return: Integer 
+        public int lowerCountMethod(List<int>values)
+        {
+            int lowerCount = values[4];
+            return lowerCount;
+        }
+
+    }
+}
